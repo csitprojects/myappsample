0ae2f9618ebb9a77c131b3fc12c9740634e7aedb
rohith0806 <rohith4204015@gmail.com>
2022-11-17 11:31:13 +0530

    second commit

--- src/Components/ImageGrid.js
+++ src/Components/ImageGrid.js
@@ -4,7 +4,7 @@ import ImageCard from "./ImageCard";
 function ImageGrid() {
   return (
     <div className="aaa">
-      <div className="innernav fixed-top"><h2 className="talign">Select a Girl</h2></div>
+      <div className="innernav fixed-top"><h2 className="talign">Select an Item</h2></div>
       <div className="containera">
         <ImageCard />
         <ImageCard />
